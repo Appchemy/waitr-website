fda6c413a61e1d785697315bd9f2358089335167
Melvin Musehani <melvin@appchemy.co.za>
2020-08-09 15:48:07 +0200

    Sign Up

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import PageLayout from '../templates/page-layout'
 import { navigate } from 'gatsby'
+import { Formik } from 'formik'
 
 const Pricing = () => {
     const [contactForm, setContactForm] = useState(null)
@@ -21,26 +22,18 @@ const Pricing = () => {
             .join("&")
     }
 
-    const submit = event => {
-        event.preventDefault()
+    const submit = values => {
         const form = contactForm
         fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode({
             "form-name": form.getAttribute("name"),
-            ...formValues,
+            ...values,
         }),
         })
         .then(() => navigate("/success-signup"))
         .catch(error => alert(error))
-
-        setFormValues({
-            firstname: 'Melvin',
-            lastname: 'Musehani',
-            phone: '0715544918',
-            email: 'melvin@appchemy.co.za'
-        })
     }
 
     return (
@@ -52,43 +45,53 @@ const Pricing = () => {
         ]}>
             <h1>Sign Up</h1>
             <p><strong>#WhyWait</strong> for better days? Sign up for a <strong>FREE</strong> trial to enjoy 30 days of no admin work while Waitr helps you to improve your business. No need to take our word for it, let your customers give you feedback, and if it doesn’t work out, we will let you run your business as you used to. </p>
-            <div>
-            <form ref={ref => {
-                if (ref) {
-                    setContactForm(ref)
-                }
-            }} method="POST" netlify-honeypot="bot-field" class="w3layouts-contact-fm" data-netlify="true" name="Sign Up" netlify>
-                <input type="hidden" name="form-name" value="Contact Form" />
-                <div class="row main-cont-sec">
-                    <div class="col-lg-6 left-cont-contact">
-                        <div class="form-group input-gap">
-                            <input class="form-control" type="text" name="firstName" id="w3lName" placeholder="First Name"
-                                required="" />
-                        </div>
-                        <div class="form-group input-gap">
-                            <input class="form-control" type="text" name="lastName" id="w3lName" placeholder="Last Name"
-                                required="" />
-                        </div>
-                        <div class="form-group input-gap">
-                            <input class="form-control" type="email" name="email" id="w3lSender" placeholder="Email"
-                                required="" />
-                        </div>
-                        <div class="form-group input-gap">
-                            <input class="form-control" type="tel" name="phone" id="w3lSender" placeholder="Phone Number"
-                                required="" />
-                        </div>
-                        <div class="form-group-2">
-                            <button onClick={submit} class="btn action-button mt-3">Sign Me Up</button>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 right-cont-contact">
-                        <div class="form-group">
-                            <img src='/assets/images/b11.png' alt='waitr-sign-up' />
-                        </div>
-                    </div>
-                </div>
-               
-            </form>
+            <div style={{marginTop: 20}}>
+            <Formik initialValues={formValues} onSubmit={submit}>
+                {({
+                    values,
+                    handleBlur,
+                    handleChange
+                }) => {
+                    return (
+                        <form ref={ref => {
+                            if (ref) {
+                                setContactForm(ref)
+                            }
+                        }} method="POST" netlify-honeypot="bot-field" class="w3layouts-contact-fm" data-netlify="true" name="Sign Up" netlify>
+                            <input type="hidden" name="form-name" value="Contact Form" />
+                            <div class="row main-cont-sec">
+                                <div class="col-lg-6 left-cont-contact">
+                                    <div class="form-group input-gap">
+                                        <input class="form-control" type="text" onBlur={handleBlur} onChange={handleChange} value={values.firstname} name="firstname" id="w3lName" placeholder="First Name"
+                                            required="" />
+                                    </div>
+                                    <div class="form-group input-gap">
+                                        <input class="form-control" type="text" onBlur={handleBlur} onChange={handleChange} value={values.lastname} name="lastname" id="w3lName" placeholder="Last Name"
+                                            required="" />
+                                    </div>
+                                    <div class="form-group input-gap">
+                                        <input class="form-control" type="email" onBlur={handleBlur} onChange={handleChange} value={values.email} name="email" id="w3lSender" placeholder="Email"
+                                            required="" />
+                                    </div>
+                                    <div class="form-group input-gap">
+                                        <input class="form-control" type="tel" name="phone" onBlur={handleBlur} onChange={handleChange} value={values.phone} id="w3lSender" placeholder="Phone Number"
+                                            required="" />
+                                    </div>
+                                    <div class="form-group-2">
+                                        <button type='submit' class="btn action-button mt-3">Sign Me Up</button>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 right-cont-contact">
+                                    <div class="form-group">
+                                        <img src='/assets/images/b11.png' alt='waitr-sign-up' />
+                                    </div>
+                                </div>
+                            </div>
+                           
+                        </form>
+                    )
+                }}
+            </Formik>
             </div>
         </PageLayout>
     )
--- src/pages/success-signup.js
+++ src/pages/success-signup.js
@@ -10,6 +10,9 @@ const SuccessSignUp = () => {
             }
         ]}>
             <h1>Thank You</h1>
+            <p>
+                The best time is now, and you know that. Congratulations on successfully signing up. We will give you a call to setup up your account. 
+            </p>
         </PageLayout>
     )
 }
